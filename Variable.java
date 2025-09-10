cebaefe3561324ad9145f4a382e6038e3c9992b4
vermaaman02 <amanver055@gmail.com>
2025-09-11 02:36:57 +0530

    variables in java

---
+++ Variable.java
@@ -0,0 +1,15 @@
+public class Variable {
+    public static void main(String[] args) {
+        int myNum = 5;               // Integer (whole number)
+        float myFloatNum = 5.99f;   // Floating point number
+        char myLetter = 'D';        // Character
+        boolean myBool = true;      // Boolean
+        String myText = "Hello";    // String
+
+        System.out.println("Integer: " + myNum);
+        System.out.println("Float: " + myFloatNum);
+        System.out.println("Character: " + myLetter);
+        System.out.println("Boolean: " + myBool);
+        System.out.println("String: " + myText);
+    }
+}
